95de1177c3deb91b9f14b98c5abb45933e8111f8
vapon <valentinponochevniy@gmail.com>
2018-03-22 22:06:06 +0300

    djusted token.

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const TelegramBot = require('node-telegram-bot-api');
 const args = process.argv;
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '';
+if (!process.env.TOKEN) {
+    console.log('setting token from args.');
+    process.env.TOKEN = process.argv[2];
+}
+
+const token = process.env.TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
